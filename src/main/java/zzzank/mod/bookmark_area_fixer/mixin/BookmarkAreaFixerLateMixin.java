dd3a151cce65d4af00fe5e5ab8bbb817277d3593
ZZZank <3410764033@qq.com>
2024-08-16 15:15:20 +0800

    setup mixin

---
+++ src/main/java/zzzank/mod/bookmark_area_fixer/mixin/BookmarkAreaFixerLateMixin.java
@@ -0,0 +1,23 @@
+package zzzank.mod.bookmark_area_fixer.mixin;
+
+import zone.rong.mixinbooter.ILateMixinLoader;
+import zzzank.mod.bookmark_area_fixer.Tags;
+import zzzank.mod.bookmark_area_fixer.mods.ModState;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author ZZZank
+ */
+public class BookmarkAreaFixerLateMixin implements ILateMixinLoader {
+    @Override
+    public List<String> getMixinConfigs() {
+        return Arrays
+            .stream(Tags.MIXIN_CONFIGS.split(" "))
+            .filter(ModState::modloaded)
+            .map(id -> String.format(Tags.MIXIN_CONFIG_FORMAT, id))
+            .collect(Collectors.toList());
+    }
+}
