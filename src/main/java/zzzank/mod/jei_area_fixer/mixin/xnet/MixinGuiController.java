bc5bbda3439b85f79e5876eba7dc46646b3c9ca3
ZZZank <3410764033@qq.com>
2024-08-29 12:05:42 +0800

    a slightly cleaner XNet Controller extra area implementation

---
+++ src/main/java/zzzank/mod/jei_area_fixer/mixin/xnet/MixinGuiController.java
@@ -0,0 +1,32 @@
+package zzzank.mod.jei_area_fixer.mixin.xnet;
+
+import mcjty.lib.gui.GenericGuiContainer;
+import mcjty.lib.gui.widgets.WidgetList;
+import mcjty.xnet.blocks.controller.TileEntityController;
+import mcjty.xnet.blocks.controller.gui.GuiController;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import zzzank.mod.jei_area_fixer.JEIAreaFixer;
+import zzzank.mod.jei_area_fixer.JEIAreaProvider;
+
+import java.awt.*;
+import java.util.List;
+
+/**
+ * @author ZZZank
+ */
+@Mixin(GuiController.class)
+public abstract class MixinGuiController extends GenericGuiContainer<TileEntityController> implements JEIAreaProvider {
+
+    @Shadow(remap = false)
+    private WidgetList connectorList;
+
+    @Override
+    public List<Rectangle> jei_area_fixer$getAreas() {
+        return JEIAreaFixer.nullableWrap(this.connectorList.getBounds());
+    }
+
+    private MixinGuiController() {
+        super(null, null, null, null, 0, null);
+    }
+}
--- src/main/java/zzzank/mod/jei_area_fixer/mods/xnet/XNetControllerArea.java
+++ src/main/java/zzzank/mod/jei_area_fixer/mods/xnet/XNetControllerArea.java
@@ -1,9 +1,8 @@
 package zzzank.mod.jei_area_fixer.mods.xnet;
 
 import mcjty.xnet.blocks.controller.gui.GuiController;
-import mezz.jei.api.gui.IAdvancedGuiHandler;
 import zzzank.mod.jei_area_fixer.AbstractJEIAreaProvider;
-import zzzank.mod.jei_area_fixer.JEIAreaFixer;
+import zzzank.mod.jei_area_fixer.JEIAreaProvider;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -22,8 +21,6 @@ public class XNetControllerArea extends AbstractJEIAreaProvider<GuiController> {
     @Nullable
     @Override
     public List<Rectangle> getExtraAreas(@Nonnull GuiController gui) {
-        var connectorList = ((GuiControllerAccessor) gui).get$connectorList();
-        var bound = connectorList.getBounds();
-        return JEIAreaFixer.nullableWrap(bound);
+        return ((JEIAreaProvider) gui).jei_area_fixer$getAreas();
     }
 }
