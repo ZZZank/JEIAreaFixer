ee0c36758d0d796160cde67d71468a6b330cc548
ZZZank <3410764033@qq.com>
2024-09-02 17:24:49 +0800

    turns out to be forestry's problem

---
+++ src/main/java/zzzank/mod/jei_area_fixer/mixin/forestry/MixinLedger.java
@@ -0,0 +1,56 @@
+package zzzank.mod.jei_area_fixer.mixin.forestry;
+
+import forestry.core.gui.ledgers.Ledger;
+import forestry.core.gui.ledgers.LedgerManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.awt.*;
+
+/**
+ * fix stupid Ledger area calculation ignoring left side situation
+ *
+ * @author ZZZank
+ */
+@Mixin(Ledger.class)
+public abstract class MixinLedger {
+
+    @Shadow
+    @Final
+    protected LedgerManager manager;
+    @Shadow
+    protected float currentWidth;
+    @Shadow
+    protected float currentHeight;
+    @Shadow
+    private int x;
+    @Shadow
+    private int y;
+
+    @Unique
+    private boolean jeiAreaFixer$atRight;
+
+    @Inject(method = "<init>(Lforestry/core/gui/ledgers/LedgerManager;Ljava/lang/String;Z)V", at = @At("RETURN"))
+    public void captureVariable(LedgerManager manager, String name, boolean rightSide, CallbackInfo ci) {
+        jeiAreaFixer$atRight = rightSide;
+    }
+
+    @Inject(method = "getArea", at = @At("HEAD"), cancellable = true)
+    public void fixLeftLedgerArea(CallbackInfoReturnable<Rectangle> cir) {
+        if (!jeiAreaFixer$atRight) {
+            final var gui = this.manager.gui;
+            cir.setReturnValue(new Rectangle(
+                gui.getGuiLeft() + this.x - gui.width - (int) this.currentWidth,
+                gui.getGuiTop() + this.y,
+                (int) this.currentWidth,
+                (int) this.currentHeight
+            ));
+        }
+    }
+}
