8727115738b123a00fe4e2972441a254da29e409
ZZZank <3410764033@qq.com>
2024-09-03 23:52:36 +0800

    directly provide area of toplevel window for XNet Controller

--- src/main/java/zzzank/mod/jei_area_fixer/mixin/xnet/MixinGuiController.java
+++ src/main/java/zzzank/mod/jei_area_fixer/mixin/xnet/MixinGuiController.java
@@ -1,11 +1,9 @@
 package zzzank.mod.jei_area_fixer.mixin.xnet;
 
 import mcjty.lib.gui.GenericGuiContainer;
-import mcjty.lib.gui.widgets.WidgetList;
 import mcjty.xnet.blocks.controller.TileEntityController;
 import mcjty.xnet.blocks.controller.gui.GuiController;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import zzzank.mod.jei_area_fixer.JEIAreaFixer;
 import zzzank.mod.jei_area_fixer.JEIAreaProvider;
 
@@ -16,16 +14,13 @@ import java.util.List;
 /**
  * @author ZZZank
  */
-@Mixin(GuiController.class)
+@Mixin(value = GuiController.class, remap = false)
 public abstract class MixinGuiController extends GenericGuiContainer<TileEntityController> implements JEIAreaProvider {
-
-    @Shadow(remap = false)
-    private WidgetList connectorList;
 
     @Nullable
     @Override
     public List<Rectangle> jei_area_fixer$getAreas() {
-        return JEIAreaFixer.nullableWrap(this.connectorList.getBounds());
+        return JEIAreaFixer.nullableWrap(this.window.getToplevel().getBounds());
     }
 
     private MixinGuiController() {
