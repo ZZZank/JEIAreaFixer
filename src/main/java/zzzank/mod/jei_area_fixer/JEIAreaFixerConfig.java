db92b988829418a1c73516fb07c451f359d89b5f
ZZZank <3410764033@qq.com>
2024-09-04 00:17:33 +0800

    more config comments

--- src/main/java/zzzank/mod/jei_area_fixer/JEIAreaFixerConfig.java
+++ src/main/java/zzzank/mod/jei_area_fixer/JEIAreaFixerConfig.java
@@ -16,6 +16,7 @@ public class JEIAreaFixerConfig {
 
     @Config.Comment("print changed bounds to log every few seconds")
     public static boolean debug$print = false;
+    @Config.Comment("Draw areas provided by JEI Area Fixer onto the screen")
     public static boolean debug$drawing = false;
 
     @Config.RequiresMcRestart
@@ -23,22 +24,29 @@ public class JEIAreaFixerConfig {
     public static boolean General$Buttons = false;
 
     @Config.RequiresMcRestart
+    @Config.Comment("General support for mods made by mcjty, mostly affecting XNet")
     public static boolean McjtyLib$General = true;
 
     @Config.RequiresMcRestart
+    @Config.Comment("Support for armor slots in extended player inventory")
     public static boolean Cyclic$ExtendedInventory = true;
 
     @Config.RequiresMcRestart
+    @Config.Comment("Support for tabs in Forestry machines")
     public static boolean Forestry$Tabs = true;
 
     @Config.RequiresMcRestart
+    @Config.Comment("Global switch for RFTools support, disabling this will disable all RFTools specific patches")
     public static boolean RFTools$All = true;
     @Config.RequiresMcRestart
+    @Config.Comment("Support crafting girds in Modular Storage")
     public static boolean RFTools$ModularStorage = true;
     @Config.RequiresMcRestart
+    @Config.Comment("Support crafting girds in Storage Scanner")
     public static boolean RFTools$StorageScanner = true;
 
     @Config.RequiresMcRestart
+    @Config.Comment("Support for Gambling Style")
     public static boolean GamblingStyle$All = true;
 
     @Config.RequiresMcRestart
@@ -46,16 +54,21 @@ public class JEIAreaFixerConfig {
     public static boolean OpenMods$All = true;
 
     @Config.RequiresMcRestart
+    @Config.Comment("Support for Quark Chest Buttons")
     public static boolean Quark$ChestButtons = true;
 
     @Config.RequiresMcRestart
+    @Config.Comment("Support for Reskillable player inventory tabs")
     public static boolean Reskillable$Tabs = true;
 
     @Config.RequiresMcRestart
+    @Config.Comment("Global switch for Smeltery IO support, disabling this will disable all Smeltery IO specific patches")
     public static boolean SmelteryIO$All = true;
     @Config.RequiresMcRestart
+    @Config.Comment("Support for Fuel Controller")
     public static boolean SmelteryIO$FuelController = true;
     @Config.RequiresMcRestart
+    @Config.Comment("Support for Casting Machine")
     public static boolean SmelteryIO$CastingMachine = true;
 
     @SubscribeEvent
