c664431aef344402884e56c5527da6463a799995
ZZZank <3410764033@qq.com>
2025-03-28 23:25:26 +0800

    i hate thaumcraft

--- src/main/java/zzzank/mod/jei_area_fixer/mods/thaumcraft/FocalManipulatorArea.java
+++ src/main/java/zzzank/mod/jei_area_fixer/mods/thaumcraft/FocalManipulatorArea.java
@@ -3,10 +3,12 @@ package zzzank.mod.jei_area_fixer.mods.thaumcraft;
 import lombok.val;
 import thaumcraft.client.gui.GuiFocalManipulator;
 import zzzank.mod.jei_area_fixer.AbstractJEIAreaProvider;
+import zzzank.mod.jei_area_fixer.utils.AreaFilter;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -29,8 +31,21 @@ public class FocalManipulatorArea extends AbstractJEIAreaProvider<GuiFocalManipu
     protected List<Rectangle> getExclusionAreas(@Nonnull GuiFocalManipulator gui) {
         val k = (gui.width - gui.getXSize()) / 2;
         val l = (gui.height - gui.getYSize()) / 2;
-        return Collections.singletonList(
-            new Rectangle(k - 71, l - 3, 71, 239) //this.drawTexturedModalRect(k - 71, l - 3, 0, 0, 71, 239);
+        val invArea = new Rectangle(k - 71, l - 3, 71, 239); //this.drawTexturedModalRect(k - 71, l - 3, 0, 0, 71, 239);
+
+        if (!gui.inventorySlots.getSlot(0).getHasStack()) {
+            return Collections.singletonList(invArea);
+        }
+
+        val rightSideArea = new Rectangle(
+            k + 230,
+            gui.getGuiTop() + AreaFilter.JEI_BOOKMARK_TOP_HEIGHT,
+            // 71 is magic number picked without much thought
+            71,
+            // 239 is also magic number, subtracting JEI related height TWICE to avoid overlapping with
+            // JEI button at the top and search bar at the bottom
+            239 - AreaFilter.JEI_BOOKMARK_TOP_HEIGHT * 2
         );
+        return Arrays.asList(invArea, rightSideArea);
     }
 }
