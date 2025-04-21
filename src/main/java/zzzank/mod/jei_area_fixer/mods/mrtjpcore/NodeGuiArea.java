d930a1463c880d58d6cb87bce86b0fda3e4a501a
ZZZank <3410764033@qq.com>
2025-04-21 11:16:38 +0800

    draft impl

---
+++ src/main/java/zzzank/mod/jei_area_fixer/mods/mrtjpcore/NodeGuiArea.java
@@ -0,0 +1,35 @@
+package zzzank.mod.jei_area_fixer.mods.mrtjpcore;
+
+import lombok.val;
+import mrtjp.core.gui.NodeGui;
+import zzzank.mod.jei_area_fixer.AbstractJEIAreaProvider;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ZZZank
+ */
+public class NodeGuiArea extends AbstractJEIAreaProvider<NodeGui> {
+    @Nonnull
+    @Override
+    protected Class<NodeGui> getTarget() {
+        return NodeGui.class;
+    }
+
+    @Nullable
+    @Override
+    protected List<Rectangle> getExclusionAreas(@Nonnull NodeGui gui) {
+        val areas = new ArrayList<Rectangle>();
+        val itr = gui.children().iterator();
+        while (itr.hasNext()) {
+            val node = itr.next();
+            val frame = node.frame();
+            areas.add(new Rectangle(frame.x(), frame.y(), frame.width(), frame.height()));
+        }
+        return areas;
+    }
+}
